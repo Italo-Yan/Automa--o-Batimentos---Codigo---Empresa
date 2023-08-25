b4f6ed64c1d6d7f6a4df6a045f6078437cb9380c
Italo-Yan <italoyan2004@gmail.com>
2023-08-25 15:26:03 -0300

    Atualizações na lógica do código commit

--- Main.py
+++ Main.py
@@ -21,22 +21,34 @@ print(f"Diferença no número total de registros: {total_difference}")
 
 # Diferença no número de CPFs exclusivos entre os dois arquivos
 unique_cpf_difference = df_new['CPF'].nunique() - df['CPF'].nunique()
-print(f"Diferença no número de CPFs exclusivos: {unique_cpf_difference}")
 
 # Novas consignatárias no arquivo mais recente que não estavam no anterior
 new_consignatarias = set(df_new['CONSIGNATARIA']) - set(df['CONSIGNATARIA'])
-print(f"Novas consignatárias no arquivo mais recente: {new_consignatarias}")
 
 # Consignatárias do arquivo anterior que não estão no mais recente
 missing_consignatarias = set(df['CONSIGNATARIA']) - set(df_new['CONSIGNATARIA'])
-print(f"Consignatárias ausentes no arquivo mais recente: {missing_consignatarias}")
+
+# Formatando a saída
+output = []
+
+if unique_cpf_difference != 0:
+    output.append(f"Diferença no número de CPFs exclusivos: {unique_cpf_difference}")
+
+if new_consignatarias:
+    output.append(f"Novas consignatárias no arquivo mais recente: {', '.join(new_consignatarias)}")
+
+if missing_consignatarias:
+    output.append(f"Consignatárias ausentes no arquivo mais recente: {', '.join(missing_consignatarias)}")
+
+# Verificando se não há diferenças
+if not output:
+    print("Não há diferenças nas comparações.")
+else:
+    print("\n".join(output))
 
 # Identificar as linhas novas ou diferentes no arquivo mais recente
 diff_rows = df_new[~df_new.apply(tuple, 1).isin(df.apply(tuple, 1))]
 print(f"Número de linhas novas ou diferentes no arquivo mais recente: {len(diff_rows)}")
-
-# Exportar as linhas diferentes para um novo arquivo Excel
-diff_file_path = "C:\\Users\\italo.mendes\\Desktop\\Automação-Batimentos\\differences.xlsx"
 
 # Ler o arquivo "PROPOSTAS YUPPI"
 df_yuppie = pd.read_excel("C:\\Users\\italo.mendes\\Desktop\\Automação-Batimentos\\PROPOSTAS YUPPIE 25.08.xlsx")
@@ -47,6 +59,19 @@ df_yuppie = df_yuppie.rename(columns={
     "DATA DIGITAÇÃO": "Data Digitação",
     "BANCO": "Consignataria"
 })
+
+# Aplica a coloração em toda a linha com base na coluna 'Color'.
+def color_row_based_on_color_column(series):
+    color = ""
+    if series["Color"] == "red":
+        color = 'background-color: red'
+    elif series["Color"] == "green":
+        color = 'background-color: green'
+    elif series["Color"] == "yellow":
+        color = 'background-color: yellow'
+    else:
+        color = ''
+    return [color] * len(series)
 
 # Comparação e atribuição das cores
 df_yuppie["Color"] = ""
@@ -69,6 +94,6 @@ for index, row in df_yuppie.iterrows():
         else:
             df_yuppie.at[index, "Color"] = "yellow"
 
-# Exportando o arquivo com as alterações
-output_path = "C:\\Users\\italo.mendes\\Desktop\\Automação-Batimentos\\BATIMENTO COMPLETO.xlsx"
-df_yuppie.to_excel(output_path, index=False, freeze_panes=(1,0))
+# Aplicando a coloração e exportando o arquivo
+styled_yuppie = df_yuppie.style.apply(color_row_based_on_color_column, axis=1)
+styled_yuppie.to_excel("C:\\Users\\italo.mendes\\Desktop\\Automação-Batimentos\\BATIMENTO COMPLETO ESTILIZADO.xlsx", engine='openpyxl', index=False)
